78308fd67dcc1027298b955c47d64752cf0e3686
Piotr Gaiński <dociebieaniuszlem@gmail.com>
2025-06-10 15:37:42 +0200

    Implement SCENT

--- tests/training/test_rgfn_trainining.py
+++ tests/training/test_rgfn_trainining.py
@@ -5,12 +5,8 @@ from training.training_test_helpers import helper__test_training__runs_properly
 
 
 @pytest.mark.parametrize(
-    "config_override_str",
-    [
-        "",
-        "include 'configs/policies/rgfn_cost_guided.gin'",
-        "include 'configs/policies/rgfn_cost_guided.gin'\ninclude 'configs/envs/dynamic_library/dynamic_library.gin'",
-    ],
+    "config_path",
+    ["configs/rgfn_test.gin", "configs/scent_test.gin"],
 )
-def test__rgfn__trains_properly(config_override_str: str, tmp_path: Path):
-    helper__test_training__runs_properly("configs/rgfn_test.gin", config_override_str, tmp_path)
+def test__rgfn__trains_properly(config_path: str, tmp_path: Path):
+    helper__test_training__runs_properly(config_path, "", tmp_path)
